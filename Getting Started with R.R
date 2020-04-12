f994c968f3787d0b546b516a7e68733022365fd9
lt-studio <tranglek1@hotmail.com>
2020-04-12 12:16:49 +0200

    First code

---
+++ Getting Started with R.R
@@ -0,0 +1,23 @@
+#see the pathway
+getwd()
+#Basic calculation: * for multiplying
+6*8 #Ctrl + Enter to run a single command
+#Basic calculation: ^ for raising the power
+2^16
+#Basic calculation: sqrt( ) for square root function
+sqrt(2)
+#Basic Calculation: abs( ) for absolute value function
+abs(-96)
+#Get help in R: ?FunctionName
+?sqrt
+#Saving the output of a function by saving it to a variable: = OR <-
+SquareRoot2 <- sqrt(2) 
+#print the variable
+SquareRoot2 
+#Naming variable: not using space but capital letters, dots or dashes instead
+Year_Hours <- 365*24
+Year_Hours
+#see the list of all variables in the dataset: ls( )
+ls()
+#run the entire code: Ctrl+Shift+Enter 
+#run the entire code without showing the results on the Console: Ctrl+Shift+s
